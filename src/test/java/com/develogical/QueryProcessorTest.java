63946a6163691d3d7caf6a912bcef24a75fe030b
Katherine <katherine.newton@db.com>
2022-04-07 13:14:00 +0100

    Update test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -27,6 +27,6 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsAboutAusten() throws Exception {
-        assertThat(queryProcessor.process("Austen"), containsString("isdjfbihsbdf"));
+        assertThat(queryProcessor.process("Austen"), containsString("author"));
     }
 }
