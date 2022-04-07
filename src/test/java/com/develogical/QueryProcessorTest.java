9b35855e9031ff4b677edb04e12cd07a9db1cc28
Katherine <katherine.newton@db.com>
2022-04-07 13:10:30 +0100

    Add new scenario

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsAboutAusten() throws Exception {
+        assertThat(queryProcessor.process("Austen"), containsString("author"));
+    }
 }
