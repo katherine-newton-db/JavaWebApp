d25a88623ceb4bc72c61b3ab6091a5151e3dfbeb
Katherine <katherine.newton@db.com>
2022-04-07 15:59:28 +0100

    Show name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -44,6 +44,13 @@ public class QueryProcessor {
             int total = num1 + num2;
             return String.valueOf(total);
         }
+        if (query.toLowerCase().contains("plus")){
+            String[] numbers = query.split(" ");
+            int num1 = Integer.parseInt(numbers[3]);
+            int num2 = Integer.parseInt(numbers[5]);
+            int total = num1 - num2;
+            return String.valueOf(total);
+        }
         if (query.toLowerCase().contains("multiplied")){
             String[] numbers = query.split(" ");
             int num1 = Integer.parseInt(numbers[3]);
